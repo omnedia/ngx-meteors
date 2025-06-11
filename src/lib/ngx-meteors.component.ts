d4841cf309806b54cfc5490e60291014bfaa6dc1
Markus Block <markus.block@y-com-automation.de>
2025-06-11 13:59:07 +0200

    feat/update: - updated logic and version for angular v20

--- src/lib/ngx-meteors.component.ts
+++ src/lib/ngx-meteors.component.ts
@@ -1,5 +1,5 @@
-import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 
 @Component({
   selector: 'om-meteors',
@@ -7,6 +7,7 @@ import { Component, Input } from '@angular/core';
   imports: [CommonModule],
   templateUrl: "./ngx-meteors.component.html",
   styleUrl: "./ngx-meteors.component.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NgxMeteorsComponent {
   @Input("styleClass")
